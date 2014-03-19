2a4f19af52ea3e137e64825c4e8bcaa53d56c2d9
Mickey <sanchez.mickey@gmail.com>
2014-03-18 21:56:32 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module DuckDuckJson
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # assets precompile false
+    config.assets.initialize_on_precompile = false
   end
 end
